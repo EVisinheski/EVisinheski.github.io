30f105cfe8f0f985bc27d4fc0826c13c23cbfa65
EVisinheski <eduardovisinheski@gmail.com>
2022-01-04 13:15:15 +0000

    init

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,32 @@
-responsiveNav = function(){
+responsiveNav = function () {
     var x = document.getElementById("respNav");
     if (x.className === "menu") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "menu";
+        x.className = "menu";
     }
 }
-displayPage = function() {
+displayPage = function () {
     document.getElementById("loader").style.display = "none";// removes the laoder.gif from the screen
     document.getElementById("respNav").style.display = "block";//reverts the display block and reveals divs
     document.getElementById("divs").style.display = "block";
-    
+
 }
 
-loaderTimeOut = function(){
+loaderTimeOut = function () {
     var timeOut;
-    timeOut = setTimeout(displayPage, 0);//calls showPage functions after 1.5 seconds
+    timeOut = setTimeout(displayPage, 1500);//calls showPage functions after 1.5 seconds
 }
-    
 
-emailValidation = function (email){
+
+emailValidation = function (email) {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(email.value.match(mailformat))
-    {
+    if (email.value.match(mailformat)) {
         alert("Valid email address!");
         document.frmEmail.email.focus();
         return true;
     }
-    else
-    {
+    else {
         alert("You have entered an invalid email address!");
         document.frmEmail.email.focus();
         return false;
